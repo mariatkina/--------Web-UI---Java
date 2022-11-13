594ab71a4dbe93160063367f5cb9b68e657cd940
EvgeneM <e.mariatkina@gmail.com>
2022-11-13 10:43:01 +0300

    ДЗ к уроку 6 в папках: \src\main\java\Lesson6HW, \src\test\java\Lesson6HW

---
+++ WebUi/src/main/java/Lesson6HW/MainPage.java
@@ -0,0 +1,17 @@
+package Lesson6HW;
+
+
+import org.openqa.selenium.WebDriver;
+
+
+public class MainPage extends BaseView {
+
+
+    public MainPage(WebDriver driver) {
+
+        super(driver);
+    }
+
+
+
+}
